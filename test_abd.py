619c61e947ff7de4f7c2f33b665d7cd0b5155957
anees-rehman1 <xcuviruszero0901@gmail.com>
2025-11-12 22:08:07 +0500

    my first project

---
+++ test_abd.py
@@ -0,0 +1,36 @@
+import pytest
+import allure
+from allure_commons.types import AttachmentType
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+import time
+
+def setup_function():
+    global driver
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver.get("https://alnafi.com/auth/sign-in")
+    driver.maximize_window()
+
+def teardown_function():
+    driver.quit()
+
+def my_cred():
+    return [
+        ('ali@gmail.com', 'abd@123'),
+        #('abd@gmail.com', 'abd@123'),
+        #('noman@gmail.com', 'abd@123')
+    ]
+
+@pytest.mark.parametrize("username,password",my_cred())
+def test_login(username,password):
+    print("my pytest login")
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="__nuxt"]/div/div[2]/div[1]/div/form/div[1]/div/div/input').send_keys(username)
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="__nuxt"]/div/div[2]/div[1]/div/form/div[2]/div/div/input').send_keys(password)
+    time.sleep(1)
+    allure.attach(driver.get_full_page_screenshot_as_png(),name="myalnafi_sc",attachment_type=AttachmentType.PNG)
---
+++ test_verify_title.py
@@ -0,0 +1,12 @@
+import  pytest
+
+def test_verify_title():
+    expected_title = "Education benefits all"
+    actual_title = "Education "
+
+    # if actual_title == expected_title:
+    #     print("my test cases is passed ")
+    # else:
+    #     print("my test is failed ")
+
+    assert True , " WE are forcing to fail ----------------------"
